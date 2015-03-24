a999c78d3f8082d6c38b909090ec3e0640f7e87f
lynzz <lynzz168@gmail.com>
2015-03-24 17:26:37 +0800

    添加 spmignore

--- app/index.js
+++ app/index.js
@@ -73,6 +73,7 @@ var PandoraGenerator = yeoman.generators.Base.extend({
 
   projectfiles: function () {
     this.copy('editorconfig', '.editorconfig');
+    this.copy('spmignore', '.spmignore');
     this.copy('jshintrc', '.jshintrc');
     this.copy('gitignore', '.gitignore');
     this.copy('travis.yml', '.travis.yml');
